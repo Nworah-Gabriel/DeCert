2c226a34ad6048115759faf85c1aaa5ba68ca09f
blacdav <davidaniefoik@gmail.com>
2024-01-08 07:35:44 +0100

    Added the some modals

--- src/app/ul/Radio.tsx
+++ src/app/ul/Radio.tsx
@@ -1,5 +1,6 @@
 'use client'
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import Proceed from '@/components/Proceed'
 import Image from 'next/image'
 import Link from 'next/link'
 import {useRouter} from 'next/navigation'
@@ -24,39 +25,43 @@ const Radio = () => {
         }
       };
 
+      const [showModal, setShowModal] = useState<boolean>(false);
 
   return (
-    <div className='flex w-full h-[100vh] text-black bg-[#EDEDED]'>
-      <section className='overflow-hidden h-[100vh] hidden lg:block'>
-        <Image src={'/image.webp'} alt='Image' width={1200} height={100} className='h-[100vh]' />
-      </section>
+    <Fragment>
+      <div className='flex w-full h-[100vh] text-black bg-[#EDEDED]'>
+        <section className='overflow-hidden h-[100vh] hidden lg:block'>
+          <Image src={'/image.webp'} alt='Image' width={1200} height={100} className='h-[100vh]' />
+        </section>
 
-      <section className='flex flex-col w-full px-10 pt-5 lg:pt-0 lg:justify-center justify-normal'>
-        <Image src={'/Logo2.webp'} alt='Logo' width={100} height={50}  className='block lg:hidden my-5' />
-        <div>
-          <Image src={'/Logo2.webp'} alt='Logo' width={100} height={50}  className='hidden lg:block my-5 lg:mt-0' />
-          <div className='flex flex-col w-full'>
-            <h1 className='font-bold text-[2rem] lg:text-[3rem] w-full opacity-80 '>Category</h1>
-            <p className='mb-10'>Select the category you belong to</p>
-          </div>
+        <section className='flex flex-col w-full px-10 pt-5 lg:pt-0 lg:justify-center justify-normal'>
+          <Image src={'/Logo2.webp'} alt='Logo' width={100} height={50}  className='block lg:hidden my-5' />
+          <div>
+            <Image src={'/Logo2.webp'} alt='Logo' width={100} height={50}  className='hidden lg:block my-5 lg:mt-0' />
+            <div className='flex flex-col w-full'>
+              <h1 className='font-bold text-[2rem] lg:text-[3rem] w-full opacity-80 '>Category</h1>
+              <p className='mb-10'>Select the category you belong to</p>
+            </div>
 
-          <div className='flex flex-col w-full lg:ml-0'>
-            <form className='' onSubmit={handleSubmit}>
-              <div className='bg-white p-4 lg:w-11/12 rounded-md'>
-                <input type='radio' name='radio' className='mr-2'/> NFT Certificate Issuer
-              </div>
-              <div className='bg-white p-4 mt-8 lg:w-11/12 rounded-md'>
-                <input type='radio' name='radio' className='mr-2'/> NFT Certificate Claimer
-              </div>
-              <div className='flex w-full lg:w-11/12 justify-between mt-8'>
-                <Link  href={`/Details`}  className='w-1/2 lg:w-full border-2 border-purple-900 p-3 rounded-md text-purple-900 mr-2'>Back</Link>
-                <button type='submit' className='w-full bg-purple-900 border-2 border-purple-900 p-3 rounded-md text-white'>Submit</button>
-              </div>
-            </form>  
+            <div className='flex flex-col w-full lg:ml-0'>
+              <form className='' onSubmit={handleSubmit}>
+                <div className='bg-white p-4 lg:w-11/12 rounded-md'>
+                  <input type='radio' name='radio' className='mr-2'/> NFT Certificate Issuer
+                </div>
+                <div className='bg-white p-4 mt-8 lg:w-11/12 rounded-md'>
+                  <input type='radio' name='radio' className='mr-2'/> NFT Certificate Claimer
+                </div>
+                <div className='flex w-full lg:w-11/12 justify-between mt-8'>
+                  <Link  href={`/Details`}  className='w-1/2 lg:w-full border-2 border-purple-900 p-3 rounded-md text-purple-900 mr-2'>Back</Link>
+                  <button type='submit' className='w-full bg-purple-900 border-2 border-purple-900 p-3 rounded-md text-white' onClick = {() => setShowModal(true)}>Submit</button>
+                </div>
+              </form>  
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+      <Proceed isVisible ={showModal} onClose = {()=> setShowModal(false)} />
+    </Fragment>
   )
 }
 
--- src/components/Proceed.tsx
+++ src/components/Proceed.tsx
@@ -8,7 +8,7 @@ type propTypes = {
     // children: React.ReactNode;
 }
 
-const Delete: React.FC<propTypes> = ({ isVisible, onClose }) => {
+const Proceed: React.FC<propTypes> = ({ isVisible, onClose }) => {
     if (!isVisible) return null;
 
     const handleClose = (event:any) => {
@@ -16,14 +16,14 @@ const Delete: React.FC<propTypes> = ({ isVisible, onClose }) => {
     }
   return (
     <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' id="wrapper" onClick={handleClose}>
-        <section className='w-[50%] lg:w-[80%] p-5 bg-white text-black flex flex-col justify-center items-center rounded-lg'>
+        <section className='w-[50%] lg:w-[80%] p-5 bg-white text-black flex flex-col justify-center items-center text-center rounded-lg'>
             <Image src={'/pleased.svg'} alt='...' width={50} height={50} className='mb-2' />
-            <h1 className='text-xl font-bold my-5'>We are Sad you are leaving already!</h1>
-            <p>Are you sure you want to delete your account? All data would be lost if you proceed with this action</p>
-            <button className='bg-[#EB1515] text-white p-3 rounded-md mt-3'>Delete Account</button>
+            <h1 className='text-xl font-bold my-5'>YOu are doing well!!!</h1>
+            <p>You have successfully signed up with Decertify</p>
+            <button className='bg-[#EB1515] text-white p-3 rounded-md mt-3'>Proceed to login</button>
         </section>
     </div>
   )
 }
 
-export default Delete
+export default Proceed
